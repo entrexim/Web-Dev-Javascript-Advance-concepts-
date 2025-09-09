5a274ae9d8ea4e902cf3cf038921828f45854083
entrexim <you@example.com>
2025-09-09 08:19:22 +0530

    updated script.js and style.js

--- Day3/script.js
+++ Day3/script.js
@@ -1,12 +1,25 @@
 const links = document.querySelectorAll('.nav-link');
 
+
+
+
 links.forEach(link => {
   link.addEventListener('click', (event) => {
     event.preventDefault(); 
-    const page = link.getAttribute('href').slice(1) + '.html'; 
-    
-    window.location.assign(page);
-    
+    const page = link.getAttribute('href').slice(1) + '.html'; // getattribute kaam ki cheej hai
+    window.location.assign(page); // Navigate
   });
 });
 
+function setActiveLink() {
+  links.forEach(link => {
+    link.classList.remove('active'); // Clear active
+    const linkPage = link.getAttribute('href').slice(1) + '.html'; // e.g., #contact -> contact.html ()yaad rakhna ise
+    if (window.location.pathname.split('/').pop() === linkPage) {
+      link.classList.add('active'); // Set active
+    }
+  });
+}
+
+// Run on page load
+setActiveLink();
